efe823ae2728336c2633a27b6aa5519f57123bdb
Christian Fischer <christian@chfi.se>
2017-08-17 20:48:12 +0200

    Add IPFSPath show instance

--- src/IPFS/Types.purs
+++ src/IPFS/Types.purs
@@ -1,6 +1,7 @@
 module IPFS.Types where
 
 import Control.Monad.Eff (kind Effect)
+import Prelude (class Show)
 
 foreign import data IPFS :: Type
 foreign import data IPFSEff :: Effect
@@ -8,3 +9,6 @@ foreign import data IPFSEff :: Effect
 
 -- TODO multihash support
 data IPFSPath = IPFSPathString String
+
+instance showIPFSPath :: Show IPFSPath where
+  show (IPFSPathString s) = s
